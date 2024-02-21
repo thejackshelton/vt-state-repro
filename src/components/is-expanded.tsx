3334f008ea1a7f261f3c9e8fa9b01128355731de
thejackshelton <me@jackshelton.com>
2024-02-20 22:40:04 -0600

    update repro

--- src/components/is-expanded.tsx
+++ src/components/is-expanded.tsx
@@ -1,14 +1,11 @@
 import { $, component$, useSignal } from "@builder.io/qwik";
 
-export const IsExpanded = component$(() => {
-  const isExpanded = useSignal<boolean>(false);
+export const Counter = component$(() => {
+  const count = useSignal<number>(0);
 
-  return (
-    <button
-      onClick$={$(() => (isExpanded.value = !isExpanded.value))}
-      aria-expanded={isExpanded.value}
-    >
-      {isExpanded.value ? "true" : "false"}
-    </button>
-  );
+  const handleClick$ = $(() => {
+    count.value++;
+  });
+
+  return <button onClick$={handleClick$}>{count.value}</button>;
 });
